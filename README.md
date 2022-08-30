8041c393c666aea50bb53b7e35f79fee0883ebcd
legojesus <yaronexp@gmail.com>
2022-08-30 14:54:52 +0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# treats-dispenser
-A python app that connects to Discord and an Arduino device with a servo motor, allowing you to chat with your pet and give it treats via chat command.
+# Treats Dispenser:
+A python app that connects to Discord and an Arduino device with a servo motor, 
+allowing you to video chat with your pet and give it treats via chat command.
+
+Tested and built on Ubuntu 20.04 and Arduino Uno.
+
+
+## Concept: 
+I always wanted to be able to see my cat and give it treats while I'm at work or travelling,
+but all existing solutions on the market are really expensive (minimum $250). I decided to build
+my own solution using an old laptop that is not in use and an Arduino unit with a motor. 
+
+The laptop is on 24/7 and connected to my Discord server in video chat, which allows me to connect
+to the same video chat room from my phone or another computer, see my cat and write the word "treat"
+in the chat to give it a treat!
+
+Total cost of the solution: ~$50 for the Arduino unit and the motor + time and creativity for
+building the app and treats container. 
+
+
+### Requirements: 
+- An active Discord account: www.discord.com 
+- An Arduino Uno unit: https://www.arduino.cc/en/Guide/ArduinoUno
+- A Servo motor unit: https://arduinogetstarted.com/tutorials/arduino-servo-motor
+
+
+### How to build and deploy:
+1. Connect the Arduino unit to the computer. 
+2. Find the used port's name/number using the official Arduino IDE app (from their official website).
+3. Edit the port variable in the code accordingly.
+4. Create a new app in Discord: https://discord.com/developers/applications
+5. Create a bot and get an API key, then invite the bot into your Discord server/room.
+6. Put the bot's API key in the last line of the code where the "xxxxxxx" part is.
+7. Connect the servo motor to the Arduino unit as shown here: https://arduinogetstarted.com/images/tutorial/arduino-servo-motor-wiring-diagram.jpg
+8. Run the code and test by writing the word "treat" in the discord chat. If all is well the motor shuld move and the bot will say it sent a treat!
+9. Use your creativity to design/build a container for the treats that the motor will be connected to and act as a dispenser on command. 
+
---
+++ treats_dispenser.py
@@ -0,0 +1,69 @@
+### Pet Dispenser 1.0
+# Author: Yaron Kachalon
+# Date: 2022-08-30
+
+# -------------------------------------------
+# This app launches a Discord bot that listens to certain keywords and activates a servo motor on an Arduino
+# device that releases a treat for your pet. Tested on Ubuntu 20.04 with Pycharm and Arduino Uno.
+# -------------------------------------------
+
+# Arduino Control:
+# Must change permissions on Arduino port so that this program can access it: sudo chmod 666 /dev/ttyACM0
+
+from pyfirmata import Arduino, SERVO
+from time import sleep
+
+port = '/dev/ttyACM0'  # Port may be different on your setup (depending on OS). Use the official Arduino IDE to find it.
+pin = 9               # The physical pin # on the Arduino in which the servo motor is connected to.
+board = Arduino(port)  # Initialize the board.
+
+board.digital[pin].mode = SERVO
+
+
+def rotateServo(pin_num: int, angle: int):
+    """
+    Spins the servo motor via the Arduino Uno unit.
+
+    Parameters
+    ----------
+    pin_num - The number of the physical pin to which the motor is connected on the Arduino board.
+    angle - The direction in which the motor will spin. 0 = counter-clockwise, 90 = stop, 180 = clockwise.
+    """
+
+    board.digital[pin_num].write(angle)
+    sleep(0.015)
+
+
+rotateServo(pin, 90)  # Keeps the servo motor off.
+
+
+# Discord Bot & Control:
+# Must have a Discord user account and create a new application bot to get an API key.
+# Reference: https://discord.com/developers/applications
+# Once the bot is created and an API key obtained, invite the bot to your Discord server before running this app.
+
+import discord
+
+intents = discord.Intents.all()
+intents.members = True
+
+client = discord.Client(intents=intents)    # Initializes the bot.
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('treat'):     # If we write the word "treat" in the chat in Discord, this function will run.
+        for i in range(0, 90):
+            rotateServo(pin, i)
+        await message.channel.send('Sent a treat to your pet!')
+
+
+client.run('xxxxxxxxx')    # The bot's API key
